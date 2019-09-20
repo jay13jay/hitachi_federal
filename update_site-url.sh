35ddbfc5df489fdb7838c05f7cf9ba1d0e0bb1e2
jHacks <josh.hackney@gmail.com>
2019-09-20 11:22:35 -0400

    updated db script to take cli args

--- update_site-url.sh
+++ update_site-url.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mysql -u wordpress -p12345678 wordpress -h hwo3cs8o51mipc.csgkuqrqolop.us-east-1.rds.amazonaws.com -e "up date wp_options set option_value = 'http://Hitachi-F-ElasticL-FBMK7YN9WPP6-927528639.us-east-1.elb.amazonaws.com' where option_id = 1;"
-mysql -u wordpress -p12345678 wordpress -h hwo3cs8o51mipc.csgkuqrqolop.us-east-1.rds.amazonaws.com -e "up date wp_options set option_value = 'http://Hitachi-F-ElasticL-FBMK7YN9WPP6-927528639.us-east-1.elb.amazonaws.com' where option_id = 2;"
+mysql -u $1 -p$2 -h $3 $4 -e "update wp_options set option_value = \'$5\' where option_id = 1;"
+mysql -u $1 -p$2 -h $3 $4 -e "update wp_options set option_value = \'$5\' where option_id = 2;"
