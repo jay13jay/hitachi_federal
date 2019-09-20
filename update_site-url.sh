cc65378f4e7ef15192439515dbae34672eeaa97e
jHacks <josh.hackney@gmail.com>
2019-09-20 11:59:10 -0400

    changed single to double quotes in updatedb script

--- update_site-url.sh
+++ update_site-url.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mysql -u $1 -p$2 -h $3 $4 -e "update wp_options set option_value = \'$5\' where option_id = 1;"
-mysql -u $1 -p$2 -h $3 $4 -e "update wp_options set option_value = \'$5\' where option_id = 2;"
+mysql -u $1 -p$2 -h $3 $4 -e "update wp_options set option_value = \"$5\" where option_id = 1;"
+mysql -u $1 -p$2 -h $3 $4 -e "update wp_options set option_value = \"$5\" where option_id = 2;"
